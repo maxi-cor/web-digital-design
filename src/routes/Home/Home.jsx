0f9d30cf4856ec6b33bb8eaa5ce16bcc66ff6ef9
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-04-04 10:56:43 -0300

    changes in home, adding the banner

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Navbrowser from "../../components/Navbrowser/Navbrowser";
 import './Home.css'
+import bannerHome from '../../assets/bannerHome.jpeg'
 
 export default function Home() {
     return (
         <div className="container_home">
             <Navbrowser/>
-            <h4>Home</h4>
+            <div className="container-banner-home">
+                <img src={bannerHome} alt="banner de inicio" />
+            </div>
         </div>
     )
 }
