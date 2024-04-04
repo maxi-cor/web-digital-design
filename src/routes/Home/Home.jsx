beb451dec20eed7d77b3898895c3eaafc14d2906
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-04-04 17:28:19 -0300

    making second part home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -8,6 +8,14 @@ export default function Home() {
             <Navbrowser/>
             <div className="container-banner-home">
                 <img src={bannerHome} alt="banner de inicio" />
+                <div className="container-text-inside-bannerContainer">
+                    <h1>Transforma tu visión en realidad digital.</h1>
+                    <p>Convertimos ideas en experiencias digitales impactantes. Con nuestro equipo de expertos en desarrollo web, te ayudaremos a destacarte en línea y alcanzar tus objetivos.</p>
+                    <p>Descubre cómo podemos ayudarte a llevar tu presencia en línea al siguiente nivel.</p>
+                </div>
+            </div>
+            <div className="container-box-recomendation">
+
             </div>
         </div>
     )
