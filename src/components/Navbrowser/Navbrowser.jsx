0460fcbd61446eefd33595b97506091f6da84e48
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-04-11 17:18:51 -0300

    more changes in home

--- src/components/Navbrowser/Navbrowser.jsx
+++ src/components/Navbrowser/Navbrowser.jsx
@@ -6,16 +6,16 @@ import './Navbrowser.css';
 
 export default function Navbrowser(){
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-dark-subtle ">
+        <Navbar collapseOnSelect expand="lg" className="bg-dark">
           <Container>
-            <Navbar.Brand style={{marginRight: '100px'}} href="/">World Digital Design</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav " />
+            <Navbar.Brand style={{marginRight: '100px', color: 'white'}} href="/">World Digital Design</Navbar.Brand>
+            <Navbar.Toggle style={{backgroundColor: "white"}} aria-controls="responsive-navbar-nav " />
             <Navbar.Collapse id="responsive-navbar-nav ">
               <Nav className="me-auto">
-                <NavLink className="nav-link d-flex justify-content-center" aria-current="page" to={"/"}>Inicio</NavLink>
-                <NavLink to={"/acerca-de"} className="nav-link d-flex justify-content-center">Acerca De</NavLink>
-                <NavLink to={"/portfolio"} className="nav-link d-flex justify-content-center">Portfolio</NavLink>
-                <NavLink to={"/contacto"} className="nav-link d-flex justify-content-center">Contacto</NavLink>
+                <NavLink activeclassname="active" className="nav-link d-flex justify-content-center " aria-current="page" to={"/"}>Inicio</NavLink>
+                <NavLink activeclassname="active" to={"/acerca-de"} className="nav-link d-flex justify-content-center">Acerca De</NavLink>
+                <NavLink activeclassname="active" to={"/portfolio"} className="nav-link d-flex justify-content-center">Portfolio</NavLink>
+                <NavLink activeclassname="active" to={"/contacto"} className="nav-link d-flex justify-content-center">Contacto</NavLink>
               </Nav>
               <Nav>
                 <Nav.Link className='d-flex justify-content-center' href="#deets">Instagram</Nav.Link>
